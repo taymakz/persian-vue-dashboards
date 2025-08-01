57c1563797b0db07ff3ac274c5081e238ff3a0f1
Taymakz <taymazak1382@gmail.com>
2025-08-01 08:46:12 +0330

    fix: add responsive padding to main layout for small screens

--- sidebar-rtl/app/layouts/account.vue
+++ sidebar-rtl/app/layouts/account.vue
@@ -2,7 +2,7 @@
 <template>
   <div class="flex h-svh w-full overflow-hidden bg-card">
     <AppSidebar />
-    <main id="main" class="relative min-h-svh overflow-y-auto h-full bg-background  lg:rounded-s-4xl lg:border-s w-full grow">
+    <main id="main" class="relative min-h-svh overflow-y-auto h-full bg-background max-lg:pt-14  lg:rounded-s-4xl lg:border-s w-full grow">
       <div class="w-full">
         <AppLayoutPageHeader>
           <AppLayoutPageTitle class="max-w-screen-lg container">
