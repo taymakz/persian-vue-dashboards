bc8f6a984f4c9ed5d1aa0ded2e2b47ed3d14b071
Taymakz <taymazak1382@gmail.com>
2025-08-01 13:21:51 +0330

    feat: add media selection to fields page

--- sidebar-rtl/app/pages/fields.vue
+++ sidebar-rtl/app/pages/fields.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import type { MediaType } from '~/types/media'
 import { computed, ref } from 'vue'
 import { toast } from 'vue-sonner'
 import SelectCity from '~/components/app/fields/SelectCity.vue'
@@ -6,6 +7,7 @@ import Button from '~/components/ui/button/Button.vue'
 import Card from '~/components/ui/card/Card.vue'
 import InputSecondary from '~/components/ui/input/InputSecondary.vue'
 
+const selectedMedias = ref<MediaType[]>([])
 const loading = ref<boolean>(false)
 const selectedCity = ref('')
 const selectedProvince = ref('')
@@ -43,6 +45,7 @@ async function submit(_values: any) {
         <Card class="w-full p-6">
           <Form v-slot="{ meta }" @submit="submit">
             <div class="space-y-4 mb-6">
+              <AppMediasMediaSelect v-model="selectedMedias" multiple />
               <Field v-slot="{ field }" name="email" validate-on-blur>
                 <InputSecondary
                   v-bind="field"
