28d759ee8a1bbd4d71cc4a60d1626a1f7021e77e
Taymakz <taymazak1382@gmail.com>
2025-08-01 08:50:35 +0330

    fix: replace useStorage with ref for mobile sidebar state management and reset on route change

--- sidebar-rtl/app/components/app/sidebar/index.vue
+++ sidebar-rtl/app/components/app/sidebar/index.vue
@@ -1,24 +1,42 @@
 <script setup lang="ts">
-import { useStorage } from '@vueuse/core'
 import { motion } from 'motion-v'
 import Content from './Content.vue'
 import Theme from './theme.vue'
 import UserOverview from './UserOverview.vue'
 
-const mobileState = useStorage('mobile-sidebar-state', false)
+const mobileState = ref(false)
+const route = useRoute()
+watch(
+  route,
+  () => {
+   mobileState.value = false
+  },
+)
+
 </script>
 
 <template>
-  <div >
+  <div>
     <AnimatePresence>
-      <motion.div v-if="mobileState" :initial="{ opacity: 0 }" :animate="{ opacity: 1 }" :exit="{ opacity: 0 }"
-        class="fixed z-40 bg-black/60 backdrop-blur-xs inset-0" @click="mobileState = false" />
+      <motion.div
+        v-if="mobileState" :initial="{ opacity: 0 }" :animate="{ opacity: 1 }" :exit="{ opacity: 0 }"
+        class="fixed z-40 bg-black/60 backdrop-blur-xs inset-0" @click="mobileState = false"
+      />
     </AnimatePresence>
+    <!-- mobile -->
+    <div class="lg:hidden fixed top-0 z-30 inset-x-0   bg-card border-b">
+      <div class="container py-2 flex items-center justify-between ">
+        <button class="size-10 p-1 rounded-md border flex items-center justify-center" @click="mobileState = !mobileState">
+          <span class="icon-[material-symbols-light--menu-rounded] size-full" />
+        </button>
+      </div>
+    </div>
     <!-- overlay -->
-
-    <div :class="cn('h-svh w-70 sm:w-66 bg-card  flex flex-col overflow-hidden max-lg:fixed left-full top-0  z-50 max-lg:border-e max-lg:rounded-e-xl transition-transform duration-300',
-      { '-translate-x-full': mobileState },
-    )">
+    <div
+      :class="cn('h-svh w-70 sm:w-66 bg-card  flex flex-col overflow-hidden max-lg:fixed left-full top-0  z-50 max-lg:border-e max-lg:rounded-e-xl transition-transform duration-300',
+                 { 'max-lg:-translate-x-full': mobileState },
+      )"
+    >
       <UserOverview />
       <Content />
       <Theme />
