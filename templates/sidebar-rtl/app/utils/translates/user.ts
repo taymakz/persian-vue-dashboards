1ebf1ed9bb5082e02faaabd5ee6c71e066fc2eae
Taymakz <taymazak1382@gmail.com>
2025-08-04 12:09:17 +0330

    feat: keep one example

--- templates/sidebar-rtl/app/utils/translates/user.ts
+++ templates/sidebar-rtl/app/utils/translates/user.ts
@@ -1,41 +1,8 @@
-import type {
-  DegreeType,
-  EntryYearSemesterType,
-  EntryYearType,
-  GenderType,
-} from '~/types/user'
-
-export function translateEntryYear(value: EntryYearType): string {
-  const translations: Record<EntryYearType, string> = {
-    BEFORE_400: '۴۰۰ و ما قبل',
-    YEAR_401: '۴۰۱',
-    YEAR_402: '۴۰۲',
-    YEAR_403: '۴۰۳',
-  }
-  return translations[value] || value
-}
-
-export function translateEntryYearSemester(
-  value: EntryYearSemesterType,
-): string {
-  const translations: Record<EntryYearSemesterType, string> = {
-    MEHR: 'مهر',
-    BAHMAN: 'بهمن',
-  }
-  return translations[value] || value
-}
-
+type GenderType = "MALE" | "FEMALE"
 export function translateGender(value: GenderType): string {
   const translations: Record<GenderType, string> = {
     MALE: 'مرد',
     FEMALE: 'زن',
-  }
-  return translations[value] || value
-}
-
-export function translateDegree(value: DegreeType): string {
-  const translations: Record<DegreeType, string> = {
-    BACHELOR: 'کارشناسی',
   }
   return translations[value] || value
 }
