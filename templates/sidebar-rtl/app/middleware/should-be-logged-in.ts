b02fd23507bc77ae9c9b8e1f7c1a69d4cfab00c4
Taymakz <taymazak1382@gmail.com>
2025-08-05 20:16:27 +0330

    feat: implement should-be-logged-in middleware for session validation and token handling

--- templates/sidebar-rtl/app/middleware/should-be-logged-in.ts
+++ templates/sidebar-rtl/app/middleware/should-be-logged-in.ts
@@ -1,4 +1,33 @@
-export default defineNuxtRouteMiddleware(async (to) => {
-  // TODO
+import { decryptSession, isExpiredToken } from '~~/server/utils/session'
 
+export default defineNuxtRouteMiddleware(async (to) => {
+  if (import.meta.client)
+    return
+  const cookie = useCookie('session')
+  if (!cookie.value) {
+    return navigateTo(`/auth/login?backUrl=${to.fullPath}`)
+  }
+  const session = await decryptSession(cookie.value)
+  if (!session) {
+    // Session decryption failed, redirect to login
+    return navigateTo(`/auth/login?backUrl=${to.fullPath}`)
+  }
+  if (isExpiredToken(session.access_exp) && isExpiredToken(session.refresh_exp)) {
+    // delete session cookie
+    cookie.value = null
+    // Both access and refresh tokens are expired, redirect to login
+    return navigateTo(`/auth/login?backUrl=${to.fullPath}`)
+  }
+  if (isExpiredToken(session.access_exp)) {
+    // Access token is expired, but refresh token is valid
+    // Redirect to refresh endpoint
+    const result = await $fetch('/api/auth/refresh', {
+      method: 'POST',
+      body: { refresh: session.refresh },
+    })
+    if (!result.success) {
+      // Refresh failed, redirect to login
+      return navigateTo(`/auth/login?backUrl=${to.fullPath}`)
+    }
+  }
 })
