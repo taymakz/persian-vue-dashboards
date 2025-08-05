97aca62a68910d65fc935e4f85a7ce3a9e6a716f
Taymakz <taymazak1382@gmail.com>
2025-08-05 21:00:04 +0330

    feat: update login and refresh endpoints to use new API response type and prepare for real authentication logic

--- templates/sidebar-rtl/server/api/auth/login.post.ts
+++ templates/sidebar-rtl/server/api/auth/login.post.ts
@@ -1,11 +1,18 @@
 import type { AccountUserTokensType } from '#shared/types/account'
+import type { ApiResponseType } from '~~/shared/types/request'
 import FetchServerApi, { errorResponse } from '~~/server/utils/api'
 import { setAuthCookieSession } from '~~/server/utils/session'
 
 export default defineEventHandler(async (event) => {
   const { email, password, otp } = await readBody(event)
   try {
-    const result = await FetchServerApi<AccountUserTokensType>(event, '/account/authenticate/login/', {
+    // const result = await FetchServerApi<AccountUserTokensType>(event, '/account/authenticate/login/', {
+    //   method: 'POST',
+    //   body: { email, password, otp },
+    // })
+    
+    // TODO - Implement real authentication logic here (you can use the commented code above as a reference)
+    const result = await $fetch<ApiResponseType<AccountUserTokensType>>('/api/test/auth', {
       method: 'POST',
       body: { email, password, otp },
     })
--- templates/sidebar-rtl/server/api/auth/refresh.post.ts
+++ templates/sidebar-rtl/server/api/auth/refresh.post.ts
@@ -1,4 +1,5 @@
 import type { AccountUserTokensType } from '#shared/types/account'
+import type { ApiResponseType } from '~~/shared/types/request'
 import { readBody, setResponseStatus } from 'h3'
 import FetchServerApi, { errorResponse } from '~~/server/utils/api'
 import { setAuthCookieSession } from '~~/server/utils/session'
@@ -7,7 +8,12 @@ export default defineEventHandler(async (event) => {
   const { refresh } = await readBody(event)
   try {
     // Send refresh token to backend refresh endpoint
-    const result = await FetchServerApi<AccountUserTokensType>(event, '/account/authenticate/refresh/', {
+    // const result = await FetchServerApi<AccountUserTokensType>(event, '/account/authenticate/refresh/', {
+    //   method: 'POST',
+    //   body: { refresh },
+    // })
+    // TODO - Implement real refresh logic here (you can use the commented code above as a reference)
+    const result = await $fetch<ApiResponseType<AccountUserTokensType>>('/api/test/refresh', {
       method: 'POST',
       body: { refresh },
     })
